cecc5c1ef0244627443a88a7c3635b94e0ceff81
matech96 <gangoly96@gmail.com>
2017-03-15 16:19:29 +0100

    Rail and Car checked and commented

--- Car.java
+++ Car.java
@@ -1,17 +1,15 @@
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * Created by matech on 2017. 02. 20..
  */
 public class Car {
 
     /**
-     *
-     * @param rail
-     * @param prevRail
-     * @param next
-     * @param color
+     * Create a new Car object.
+     * @param rail The Car will stand on this Rail.
+     * @param prevRail The Car has come from this Rail.
+     * @param next In the train, the Car behind this Car.
+     * @param color The Color of the Car.
      */
     public Car(Rail rail, Rail prevRail, Car next, Color color) {
         this.rail = rail;
@@ -31,29 +29,43 @@ public class Car {
     protected boolean canEmpty;
     protected boolean empty;
 
+
     /**
-     * @return
+     * The Car moves to the next Rail in the network.
+     * @return 1 if there was a collision, 0 otherwise.
      */
     public int runTurn() {
         System.out.print(rail.id + " ");
-        int ret = 1;
+
+        int isCollision = move();
+        if (isCollision == 1) {
+            return isCollision;
+        }
+
+        return callNextCar();
+    }
+
+    private int move() {
         Rail tmp = rail;
-        rail = rail.carMoves(this, prevRail);
+        try {
+            rail = rail.carMoves(this, prevRail);
+        } catch (Exception e) {
+            return 1;
+        }
         prevRail = tmp;
+        return 0;
+    }
+
+    private int callNextCar() {
         if(next != null) {
-            int r = next.runTurn();
-            if(r == 2 && empty)
-                ret = 2;
+            return next.runTurn();
         }
-        else{
-            if(empty)
-                ret = 2;
-        }
-        return ret;
+        return 0;
     }
 
     /**
-     * @param c
+     * Sets the Cars Color to NO_COLOR, if c equals the Cars Color and canEmpty is true.
+     * @param c The Color to compare.
      */
     public void atStation(Color c) {
         if(color == c && canEmpty){
--- Rail.java
+++ Rail.java
@@ -1,14 +1,13 @@
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * Created by matech on 2017. 02. 20..
  */
 public class Rail {
 
     /**
-     * @param from
-     * @param to
+     * Create a new Rail object.
+     * @param from Connected to this Rail.
+     * @param to Connected to this Rail. If Tunnel will build this neighbor will be replaced.
      */
     public Rail(Rail from, Rail to) {
         id = idGenerator++;
@@ -21,18 +20,34 @@ public class Rail {
     protected Rail from;
     protected Rail to;
     protected Car car;
-    protected Color color;
 
     /**
-     * @param t
-     * @param prev
-     * @return
+     * Moves the Car to the next Rail in the network.
+     * @param c The Car that moves.
+     * @param prev The Rail where the Car came from.
+     * @return The Rail where c stands.
+     * @throws Exception In occasion of collision!
      */
-    public Rail carMoves(Car t, Rail prev) {
-        if(from == prev)
+    public Rail carMoves(Car c, Rail prev) throws Exception {
+        Rail nextRail = getNextRail(prev);
+        nextRail.addCar(c);
+        return nextRail;
+    }
+
+    private Rail getNextRail(Rail prev) {
+        if(from == prev) {
             return to;
-        else
+        } else {
             return from;
+        }
+    }
+
+    protected void addCar(Car c) throws Exception {
+        if (car != null) {
+            throw new Exception("Collision!");
+        } else {
+            car = c;
+        }
     }
 
     /**
