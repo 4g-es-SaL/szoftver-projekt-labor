060c326dfdc3946e907638e2f48179ed7b020a10
Sári László <slaszlo.bme@gmail.com>
2017-03-15 10:30:45 +0100

    Implemented constructor

--- Switch.java
+++ Switch.java
@@ -13,27 +13,16 @@ public class Switch extends Rail {
      * @param to
      * @param to2
      */
-    public Switch(Rail from, Rail to, Set<Rail> to2) {
+    public Switch(Rail from, Rail to, ArrayList<Rail> to2) {
         super(from, to);
         // TODO implement here
-        throw new NotImplementedException();
+        alternativeWays = to2;
     }
 
-    /**
-     * 
-     */
-    protected Set<Rail> alternativeWays;
-
-    /**
-     * 
-     */
+    protected ArrayList<Rail> alternativeWays;
     protected int currentToId = 0;
 
 
-
-    /**
-     *
-     */
     public void changeDir() {
         // TODO implement here
         throw new NotImplementedException();
