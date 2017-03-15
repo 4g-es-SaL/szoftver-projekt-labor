853ce1c0359d5f9cf4edd80ae52839efb2f8ed7c
Sári László <slaszlo.bme@gmail.com>
2017-03-15 16:34:36 +0100

    it's now according to the specification. added a clearCars() func

--- Rail.java
+++ Rail.java
@@ -24,16 +24,35 @@ public class Rail {
     protected Color color;
 
     /**
-     * @param t
-     * @param prev
-     * @return
+     * Moves the Car to the next Rail in the network.
+     * @param c The Car that moves.
+     * @param prev The Rail where the Car came from.
+     * @return The Rail where c stands.
+     * @throws Exception In occasion of collision!
      */
-    public Rail carMoves(Car t, Rail prev) {
-        if(from == prev)
-            return to;
-        else
-            return from;
+    public Rail carMoves(Car c, Rail prev) throws Exception {
+        Rail nextRail = getNextRail(prev);
+        nextRail.addCar(c);
+        return nextRail;
     }
+
+    private Rail getNextRail(Rail prev) {
+        if(from == prev) {
+            return to;
+        } else {
+            return from;
+        }
+    }
+
+    protected void addCar(Car c) throws Exception {
+        if (car != null) {
+            throw new Exception("Collision!");
+        } else {
+            car = c;
+        }
+    }
+
+    protected void clearCar(){ car = null; }
 
     /**
      * @param r
